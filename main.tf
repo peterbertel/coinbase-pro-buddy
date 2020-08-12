1232e3ecbd5c4e7147cb503b02a3f3359a6ddfe9
peterbertel <peterbertel@gmail.com>
2020-08-12 06:04:22 -0400

    Create a new IAM policy document for giving the Lambda function the ec2:CreateNetworkInterface permission

--- main.tf
+++ main.tf
@@ -13,15 +13,32 @@ resource "aws_eip" "nat" {
 resource "aws_nat_gateway" "gateway" {
   allocation_id = aws_eip.nat.id
   subnet_id     = var.subnet_id
-
-  tags = {
+  tags          = {
     Name = "Coinbase NAT Gateway"
   }
 }
 
-resource "aws_iam_role" "lambda_iam_role" {
-  name = "lambda_iam_role"
+data "aws_iam_policy_document" "coinbase_lambda_policy_doc" {
+  statement {
+    sid     = "1"
+    effect  = "Allow"
+    actions = [
+      "ec2:CreateNetworkInterface"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+}
 
+resource "aws_iam_policy" "coinbase_lambda_policy" {
+  name        = "coinbase-lambda-execution-policy"
+  description = "The IAM policy for the Coinbase Lambda function"
+  policy      = data.aws_iam_policy_document.coinbase_lambda_policy_doc.json
+}
+
+resource "aws_iam_role" "coinbase_lambda_role" {
+  name               = "coinbase-lambda-execution-role"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -39,10 +56,15 @@ resource "aws_iam_role" "lambda_iam_role" {
 EOF
 }
 
+resource "aws_iam_role_policy_attachment" "coinbase-lambda-iam-role-policy-attachment" {
+  role       = aws_iam_role.coinbase_lambda_role.name
+  policy_arn = aws_iam_policy.coinbase_lambda_policy.arn
+}
+
 resource "aws_lambda_function" "coinbase_lambda" {
   filename         = "python-scripts/lambda.zip"
   function_name    = "CoinbaseLambda"
-  role             = aws_iam_role.lambda_iam_role.arn
+  role             = aws_iam_role.coinbase_lambda_role.arn
   handler          = "get-accounts.lambda_handler"
   source_code_hash = filebase64sha256("python-scripts/lambda.zip")
   runtime          = "python3.8"
