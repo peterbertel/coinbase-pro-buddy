af125e9a186683be1bd25bb8622aba7539e50f74
peterbertel <peterbertel@gmail.com>
2020-08-15 08:11:46 -0400

    Increate lambda timeout to 5 seconds for the deposit-funds function

--- main.tf
+++ main.tf
@@ -182,9 +182,10 @@ resource "aws_lambda_function" "coinbase_lambda" {
   filename         = "python-scripts/lambda.zip"
   function_name    = "CoinbaseLambda"
   role             = aws_iam_role.coinbase_lambda_role.arn
-  handler          = "get-accounts.lambda_handler"
+  handler          = "deposit-funds.lambda_handler"
   source_code_hash = filebase64sha256("python-scripts/lambda.zip")
   runtime          = "python3.8"
+  timeout          = 5
 
   vpc_config {
     subnet_ids         = [aws_subnet.coinbase_subnet_b.id, aws_subnet.coinbase_subnet_c.id]
