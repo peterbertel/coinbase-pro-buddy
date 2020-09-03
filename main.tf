775e06f40c1b7ce4bdce61f24e423f64172e9046
peterbertel <peterbertel@gmail.com>
2020-09-03 08:08:26 -0400

    Add the order_size environment variable to the 'order' lambda function

--- main.tf
+++ main.tf
@@ -251,6 +251,12 @@ resource "aws_lambda_function" "coinbase_lambda_order" {
     subnet_ids         = [aws_subnet.coinbase_subnet_b.id, aws_subnet.coinbase_subnet_c.id]
     security_group_ids = [aws_security_group.lambda_sg.id]
   }
+
+  environment {
+    variables = {
+      ORDER_SIZE_IN_USD = var.order_size_in_usd
+    }
+  }
 }
 
 resource "aws_cloudwatch_event_rule" "lambda_deposit_event_rule" {
